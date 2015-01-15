b8068c0ef7b621d3fa11ea6e34b49d711ca64dd9
Keith Smiley <keithbsmiley@gmail.com>
2015-01-15 11:39:04 -0800

    Initial commit

---
+++ Example/Example/AppDelegate.m
@@ -0,0 +1,11 @@
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+
+@end
+
+@implementation AppDelegate
+
+@end
---
+++ Example/Example/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Example
+//
+//  Created by Keith Smiley on 1/15/15.
+//  Copyright (c) 2015 keithsmiley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
